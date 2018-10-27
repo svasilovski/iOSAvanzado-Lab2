b9d783aeed038d743f8395f0647de3b73d141a8d
SAMUEL <samuel@macbook-pro-de-samuel.local>
2018-10-27 20:24:46 -0300

    Initial Commit

---
+++ iOSAvanzado-Lab2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSAvanzado-Lab2
+//
+//  Created by Samuel Vasilovski on 27/10/18.
+//  Copyright © 2018 SAMUEL VASILOVSKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSAvanzado-Lab2/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOSAvanzado-Lab2
+//
+//  Created by Samuel Vasilovski on 27/10/18.
+//  Copyright © 2018 SAMUEL VASILOVSKI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end

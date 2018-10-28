dfddb10229ba3fd0851dc5af7fe34c0267bc3c24
SAMUEL <samuel@macbook-pro-de-samuel.local>
2018-10-28 13:34:05 -0300

    Dos archivos que no se subieron.

---
+++ iOSAvanzado-Lab2/Model/Detalle.h
@@ -0,0 +1,33 @@
+//
+//  Detalle.h
+//  iOSAvanzado-Lab2
+//
+//  Created by Samuel Vasilovski on 28/10/18.
+//  Copyright © 2018 SAMUEL VASILOVSKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Detalle : NSObject
+
+@property (nonatomic, readonly) NSString *name;
+@property (nonatomic, readonly) NSString *lastName;
+@property (nonatomic, readonly) NSString *fullName;
+@property (nonatomic, readonly) NSString *details;
+@property (nonatomic, readonly) UIImage *image;
+
+- (instancetype)initWithName:(NSString *)name
+                    lastName:(NSString *)lastName
+                    fullName:(NSString *)fullName
+                     details:(NSString *)details
+                       image:(UIImage *)image;
+
++ (Detalle *)subdivisionWithName:(NSString *)name
+                        lastName:(NSString *)lastName
+                         details:(NSString *)details
+                           image:(UIImage *)image;
+@end
+
+NS_ASSUME_NONNULL_END
